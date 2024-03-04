0b99250e11157c425fc2fab7576a87c08e826a42
Salinas <matheussalinas3@gmail.com>
2024-03-04 14:52:40 -0300

    chore: setup project :tada:

---
+++ src/app/layout.tsx
@@ -0,0 +1,23 @@
+import './globals.css'
+
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'Verse',
+  description: 'Invista seu tempo com Deus!',
+}
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  )
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="flex h-screen w-screen items-center justify-center bg-yellow-200 ">
+      <h1>Hello world, from Verse!</h1>
+    </div>
+  )
+}

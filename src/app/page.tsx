d90f7c4cc79c802ac3061f2590593aa712d7e730
Salinas <matheussalinas3@gmail.com>
2024-03-04 22:49:48 -0300

    feat: add countdown
    It's a possible way to implement this countdown, if u have a better way,
    your pr is welcome!

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
+import { Countdown } from '@/components/countdown'
 import { DailyVerse } from '@/components/daily-verse'
 
 export default function Home() {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center gap-5 antialiased">
       <h1 className="text-muted-foreground">
-        <b>02:43:16</b> para um novo versículo 🙌
+        <Countdown /> para um novo versículo 🙌
       </h1>
       <DailyVerse />
       <footer className="text-center text-sm text-muted-foreground">
---
+++ src/components/countdown.tsx
@@ -0,0 +1,55 @@
+'use client'
+
+import { addDays, differenceInSeconds, set } from 'date-fns'
+import { useEffect, useState } from 'react'
+
+export function Countdown() {
+  const now = new Date()
+
+  const tomorrow = set(addDays(now, 1), {
+    hours: 6,
+    milliseconds: 0,
+    seconds: 0,
+    minutes: 0,
+  })
+
+  const difference = differenceInSeconds(tomorrow, now)
+
+  const hour = Math.floor(difference / (60 * 60))
+  const minute = Math.floor((difference % (60 * 60)) / 60)
+  const second = Math.floor(difference % 60)
+
+  const [seconds, setSeconds] = useState(second)
+  const [minutes, setMinutes] = useState(minute)
+  const [hours, setHours] = useState(hour)
+
+  useEffect(() => {
+    seconds > 0 && setTimeout(() => setSeconds(seconds - 1), 1000)
+
+    if (seconds === 0 && minutes > 0) {
+      setMinutes(minutes - 1)
+      setSeconds(60)
+    }
+
+    if (minutes === 0 && hours > 0) {
+      setHours(hours - 1)
+      setMinutes(60)
+    }
+
+    if (hours === 0 && minutes === 0 && seconds === 0) {
+      console.log('revalidate data!')
+
+      setSeconds(60)
+      setMinutes(60)
+      setHours(2)
+    }
+  }, [seconds, minutes, hours])
+
+  return (
+    <b>
+      {hours.toString().padStart(2, '0')} :{' '}
+      {minutes.toString().padStart(2, '0')} :{' '}
+      {seconds.toString().padStart(2, '0')}
+    </b>
+  )
+}

9e471cbeafa61a5d0d0db07fda9262ac7c4b88ce
Salinas <matheussalinas3@gmail.com>
2024-05-08 17:15:18 -0300

    refactor: add card around daily verse

--- src/components/daily-verse.tsx
+++ src/components/daily-verse.tsx
@@ -30,26 +30,22 @@ export async function DailyVerse() {
   const { book, chapter, number, text } = await getVerse()
 
   return (
-    <div className="px-6 md:w-[75%] lg:w-[70%] xl:w-[50%]">
-      <Card className="m-auto max-w-md shadow-none">
-        <CardHeader className="space-y-0 p-4 pb-1">
-          <CardDescription className="text-xs">
-            Versículo do dia
-          </CardDescription>
-          <CardTitle className="text-lg">
-            {book.name} {chapter}:{number}
-          </CardTitle>
-          <CardDescription className="text-xs">
-            Escritor:{' '}
-            <span className="font-medium text-zinc-950 underline">
-              {book.author}
-            </span>
-          </CardDescription>
-        </CardHeader>
-        <CardContent className="p-4 pt-0">
-          <h1>{text.replace('"', '')}</h1>
-        </CardContent>
-      </Card>
-    </div>
+    <Card className="max-w-md shadow-none">
+      <CardHeader className="space-y-0 p-4 pb-1">
+        <CardDescription className="text-xs">Versículo do dia</CardDescription>
+        <CardTitle className="text-lg">
+          {book.name} {chapter}:{number}
+        </CardTitle>
+        <CardDescription className="text-xs">
+          Escritor:{' '}
+          <span className="font-medium text-zinc-950 underline">
+            {book.author}
+          </span>
+        </CardDescription>
+      </CardHeader>
+      <CardContent className="p-4 pt-0">
+        <h1>{text.replace('"', '')}</h1>
+      </CardContent>
+    </Card>
   )
 }

f6994fa55dcafa50fb34ef759b143f7608a1996a
Salinas <matheussalinas3@gmail.com>
2024-05-09 16:50:41 -0300

    refactor: create header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,8 @@
+import Image from 'next/image'
+
 import { Countdown } from '@/components/countdown'
 import { DailyVerse } from '@/components/daily-verse'
+import { ThemeToggle } from '@/components/theme-toogle'
 import { Card } from '@/components/ui/card'
 import { siteConfig } from '@/config/site'
 
@@ -9,9 +12,19 @@ export default async function Home() {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center antialiased">
       <Card className="flex max-w-md flex-col items-center gap-2 rounded-3xl p-8">
-        <h1 className="text-muted-foreground">
-          <Countdown /> para um novo versículo 🙌
-        </h1>
+        <header className="flex w-full justify-between">
+          <Image
+            src="/logotipo-verse.svg"
+            width={32}
+            height={32}
+            alt="Logotipo Verse"
+          />
+
+          <div className="flex items-center gap-2">
+            <Countdown />
+            <ThemeToggle />
+          </div>
+        </header>
         <DailyVerse />
         <footer className="rounded-full border bg-card px-3 py-2 text-center text-xs font-medium text-card-foreground">
           &copy; {siteConfig.name} {new Date().getFullYear()}
---
+++ src/components/theme-toogle.tsx
@@ -0,0 +1,31 @@
+'use client'
+
+import { Moon, Sun } from 'lucide-react'
+import * as React from 'react'
+
+import { Button } from '@/components/ui/button'
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu'
+
+export function ThemeToggle() {
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="outline" size="icon">
+          <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+          <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+          <span className="sr-only">Toggle theme</span>
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="start">
+        <DropdownMenuItem>Light</DropdownMenuItem>
+        <DropdownMenuItem>Dark</DropdownMenuItem>
+        <DropdownMenuItem>System</DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  )
+}

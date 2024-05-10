142ceefd963701aaa5be4f654d8511f8bfe8d504
Salinas <matheussalinas3@gmail.com>
2024-05-10 17:02:58 -0300

    feat: add `site.webmanifest`

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -63,6 +63,7 @@ export const metadata: Metadata = {
     shortcut: '/favicon-16x16.png',
     apple: '/apple-touch-icon.png',
   },
+  manifest: `${siteConfig.url}/site.webmanifest`,
 }
 
 export default function RootLayout({

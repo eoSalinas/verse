136fc16c17a6282620a014c9f637fddc679825ee
Emanuel Avila <avilaemanueel@gmail.com>
2024-08-27 21:32:24 -0300

    ci: update `next.config`

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,21 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  // Just to ensure that React is always on strict mode
+  reactStrictMode: true,
+
+  // We want to always enforce that SWC minifies the sources even during Development mode
+  // so that bundles are minified on-the-go. SWF minifying is fast, and has almost no penalties
+  swcMinify: true,
+
+  // We don't use trailing slashes on URLs from the Node.js Website
+  trailingSlash: false,
+
+  // We don't want to redirect with trailing slashes
+  skipTrailingSlashRedirect: true,
+
+  typescript: { ignoreBuildErrors: true },
+
+  eslint: { dirs: ['.'], ignoreDuringBuilds: true },
+}
 
 export default nextConfig

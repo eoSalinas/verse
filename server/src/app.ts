628d27e5fc31653190451b0040d67bb21f5c959d
Salinas <80262440+eoSalinas@users.noreply.github.com>
2025-02-08 09:45:58 -0300

    fix(kairos): remove built-in fastify logger

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,8 +1,6 @@
 import fastify from 'fastify'
 
-export const app = fastify({
-  logger: true,
-})
+export const app = fastify()
 
 app.get('/health', async function handler(_, reply) {
   return reply.status(200).send({ status: 'OK' })

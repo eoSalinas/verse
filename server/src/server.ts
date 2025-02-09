909906b81a5d59a1672824ad775b97d529aa26a6
Salinas <80262440+eoSalinas@users.noreply.github.com>
2025-02-09 10:23:16 -0300

    refactor(kairos): update to use validated env variable

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,15 +1,10 @@
-import dotenv from 'dotenv'
-
 import { app } from './app'
-
-dotenv.config()
-
-const port = Number(process.env.PORT) ?? 3030
+import { env } from './env'
 
 app
   .listen({
     host: '0.0.0.0',
-    port,
+    port: env.PORT,
   })
   .then(() => {
     console.log('🔥 HTTP Server running!')

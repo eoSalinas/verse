da0ae9fc445096d0f84ad44b52eb9f5f49670d05
Salinas <80262440+eoSalinas@users.noreply.github.com>
2025-02-12 08:26:23 -0300

    [FEATURE] Setup Prisma ORM for the backend (#53)

---
+++ server/prisma/migrations/20250212111149_create_verse_table/migration.sql
@@ -0,0 +1,9 @@
+-- CreateTable
+CREATE TABLE "verses" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "content" TEXT NOT NULL,
+    "book" TEXT NOT NULL,
+    "chapter" TEXT NOT NULL,
+    "verse" TEXT NOT NULL,
+    "author" TEXT NOT NULL
+);
